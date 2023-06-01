0c69376aa3f8252fc82fab42f6c78bfe91044629
mahmoudmohamed <mahmoudmohamed199885.com>
2023-06-01 17:25:39 +0300

    f

--- app/Http/Controllers/Api/UserCarController.php
+++ app/Http/Controllers/Api/UserCarController.php
@@ -78,19 +78,8 @@ class UserCarController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
-     */
-}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.

e676d59879f76ef4300b710d52c55bba570ef134
mahmoudmohamed <mahmoudmohamed199885.com>
2023-06-24 23:38:40 +0300

    F'

--- app/Http/Controllers/Api/WishlistUserController.php
+++ app/Http/Controllers/Api/WishlistUserController.php
@@ -63,7 +63,7 @@ class WishlistUserController extends Controller
 
     public function notWishlist($car_id)
     {
-        $wishlist =  Wishlist::whereUseId(auth()->id())
+        $wishlist =  Wishlist::whereUserId(auth()->id())
             ->whereCarId($car_id)
             ->first();
 

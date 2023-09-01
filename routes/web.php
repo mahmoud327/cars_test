4a8412e050293099eb90036b84806785b3c4b6da
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-09-01 17:53:07 +0300

    F

--- routes/web.php
+++ routes/web.php
@@ -25,49 +25,46 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
-use GuzzleHttp\Client;
+// use GuzzleHttp\Client;
 
- Route::get('test',function(){
-
-
-    $client = new Client();
-
-    $guzzleResponse = $client->get(
-        'https://sayartii.com/carmakes'
-    );
-if ($guzzleResponse->getStatusCode() == 200) {
-    $response = json_decode($guzzleResponse->getBody(),true);
+// Route::get('test', function () {
 
 
+//     $client = new Client();
 
-    // Assuming the API response is a JSON array with IDs.
-    if (is_array($response)) {
-        $ids = array_column($response, 'opts/car.make');
-        foreach($ids as $id){
-           $category= Category::create([
-                'type'=> 'make'
-            ]);
-            TranslationCategory::create([
-                'category_id'=>$category->id,
-                'name'=>$id,
-                'locale'=>'en'
-
-            ]);
-            TranslationCategory::create([
-                'category_id'=>$category->id,
-                'name'=>$id,
-                'locale'=>'ar'
-
-            ]);
-        }
-        return $ids;
-    }
-    // return $response;
-}
+//     $guzzleResponse = $client->get(
+//         'https://sayartii.com/carmakes'
+//     );
+//     if ($guzzleResponse->getStatusCode() == 200) {
+//         $response = json_decode($guzzleResponse->getBody(), true);
 
 
 
-});
+//         // Assuming the API response is a JSON array with IDs.
+//         if (is_array($response)) {
+//             $ids = array_column($response, 'opts/car.make');
+//             foreach ($ids as $id) {
+//                 $category = Category::create([
+//                     'type' => 'make'
+//                 ]);
+//                 TranslationCategory::create([
+//                     'category_id' => $category->id,
+//                     'name' => $id,
+//                     'locale' => 'en'
+
+//                 ]);
+//                 TranslationCategory::create([
+//                     'category_id' => $category->id,
+//                     'name' => $id,
+//                     'locale' => 'ar'
+
+//                 ]);
+//             }
+//             return $ids;
+//         }
+//         // return $response;
+//     }
+// });
 Route::redirect('/', 'admin/login-page');
 
 Auth::routes();
@@ -85,27 +82,25 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
         Route::resource('users', UserController::class);
         Route::resource('companies', CompanyController::class);
-        Route::post('company/{id}', [CompanyController::class,'isActive'])
-        ->name('company.is-active');
+        Route::post('company/{id}', [CompanyController::class, 'isActive'])
+            ->name('company.is-active');
 
-        Route::post('car/{id}/is-active', [CarController::class,'isActive'])
-        ->name('car.is-active');
+        Route::post('car/{id}/is-active', [CarController::class, 'isActive'])
+            ->name('car.is-active');
 
-        Route::post('company/{id}/distinguished', [CompanyController::class,'distinguished'])
-        ->name('company.is-distinguished');
+        Route::post('company/{id}/distinguished', [CompanyController::class, 'distinguished'])
+            ->name('company.is-distinguished');
 
-        Route::post('car/{id}/distinguished', [CarController::class,'distinguished'])
-        ->name('car.distinguished');
+        Route::post('car/{id}/distinguished', [CarController::class, 'distinguished'])
+            ->name('car.distinguished');
 
         Route::resource('categories', CategoryController::class);
         Route::resource('banners', BannerController::class);
         Route::resource('features', FeatureController::class);
         Route::resource('feature.listings', FeatureListingController::class);
         Route::resource('tags', TagController::class);
-        Route::resource('pages',PageController::class);
-        Route::resource('cities',CityController::class);
+        Route::resource('pages', PageController::class);
+        Route::resource('cities', CityController::class);
         Route::resource('cars', CarController::class);
-
-
     });
 });
